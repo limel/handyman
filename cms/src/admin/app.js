d04c9a326da937f0948298615635f8ece59a0929
Andrey Nazarenko <limelaudio@gmail.com>
2023-05-26 00:17:57 +0300

    add linters & initial strapi & next,js

---
+++ cms/src/admin/app.js
@@ -0,0 +1,55 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],   
+  translations: {
+    en: {
+      "Auth.form.welcome.title": "Welcome to Handyman admin panel",
+      "Auth.form.welcome.subtitle": "Enter your username and password to enter",
+      "app.components.LeftMenu.navbrand.title": "Handyman",
+      "app.components.LeftMenu.navbrand.workplace": "Admin panel",
+    },
+  },
+
+  theme: {
+    colors: {
+      primary700: "#FEF9CC",
+      primary600: "#FEF9CC",
+      alternative700: "#FEF9CC",
+    }
+  },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};

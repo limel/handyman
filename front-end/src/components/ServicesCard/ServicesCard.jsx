76a75fa7da7fb19b244c0125c94fdbbc93dc8e05
Andrey Nazarenko <limelaudio@gmail.com>
2023-05-28 20:23:15 +0300

    services list (pending)

--- front-end/src/components/ServicesCard/ServicesCard.jsx
+++ front-end/src/components/ServicesCard/ServicesCard.jsx
@@ -1,33 +1,61 @@
+import { useRef, useEffect } from 'react';
 import Image from 'next/image';
 import cn from 'classnames';
 import Button from '~/components/UI/Button';
 import s from './ServicesCard.module.scss';
 
-const ServicesCard = ({ title, description }) => (
-  <li className={ cn(s.card, s.active) }>
-    <figure className={ s.main }>
-      <figcaption>
-        <h2 className={ s.title }>{title}</h2>
-      </figcaption>
-      <div className={ s.image }>
-        <Image
-          src="/images/services/test.jpg"
-          alt="test"
-          fill
-          style={ {
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            inset: ' auto 0 0 auto',
-          } }
-        />
-      </div>
-    </figure>
-    <div className="text" dangerouslySetInnerHTML={ { __html: description } } />
-    <Button href="/services" className={ s.button }>
-      GET A QUOTE
-    </Button>
-  </li>
-);
+const ServicesCard = ({
+  title, description, isActive, onClick,
+}) =>
+{
+  const textRef = useRef(null);
+
+  useEffect(() =>
+  {
+    if (isActive) textRef.current.style.height = `${ textRef.current.scrollHeight }px`;
+    else textRef.current.style.height = '0px';
+  }, [ isActive ]);
+
+  const handleKeyPress = (e) =>
+  {
+    if (e.key === 'Enter') onClick();
+  };
+
+  const cardClassName = cn(s.card, { [s.active]: isActive });
+
+  return (
+    <li
+      className={ cardClassName }
+      onClick={ onClick }
+      tabIndex={ 0 }
+      onKeyDown={ handleKeyPress }
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+      role="button"
+    >
+      <figure className={ s.main }>
+        <figcaption>
+          <h2 className={ s.title }>{title}</h2>
+        </figcaption>
+        <div className={ s.image }>
+          <Image
+            src="/images/services/test.jpg"
+            alt="test"
+            fill
+            style={ {
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              inset: ' auto 0 0 auto',
+            } }
+          />
+        </div>
+      </figure>
+      <div ref={ textRef } className="text" dangerouslySetInnerHTML={ { __html: description } } />
+      <Button href="/services" className={ s.button }>
+        GET A QUOTE
+      </Button>
+    </li>
+  );
+};
 
 export default ServicesCard;
--- front-end/src/components/ServicesList/ServicesList.jsx
+++ front-end/src/components/ServicesList/ServicesList.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/no-array-index-key */
+import { useState } from 'react';
 import ServicesCard from '~/components/ServicesCard';
+import cn from 'classnames';
 import Title from '../UI/Title/Title';
 import s from './ServicesList.module.scss';
 
 const listPlaceholder = [
   {
     id: 1,
-    title: 'In the Bathroom',
+    title: 'In the Bathroom1',
     description: `<ul>
       <li>Vanity installation</li>
       <li>Toilet seals and total replacements</li>
@@ -18,8 +20,26 @@ const listPlaceholder = [
     </ul>`,
   },
   {
-    id: 1,
-    title: 'In the Bathroom',
+    id: 2,
+    title: 'In the Bathroom2',
+    description: `<ul>
+      <li>Vanity installation</li>
+      <li>Toilet seals and total replacements</li>
+      <li>Updating wall fixtures</li>
+      <li>Grout</li>
+      <li>Shower door installation</li>
+      <li>Replacing bathroom fans</li>
+      <li>Undersink plumbing and p-traps</li>
+      <li>Updating wall fixtures</li>
+      <li>Grout</li>
+      <li>Shower door installation</li>
+      <li>Replacing bathroom fans</li>
+      <li>Undersink plumbing and p-traps</li>
+    </ul>`,
+  },
+  {
+    id: 3,
+    title: 'In the Bathroom3',
     description: `<ul>
       <li>Vanity installation</li>
       <li>Toilet seals and total replacements</li>
@@ -31,8 +51,8 @@ const listPlaceholder = [
     </ul>`,
   },
   {
-    id: 1,
-    title: 'In the Bathroom',
+    id: 4,
+    title: 'In the Bathroom4',
     description: `<ul>
       <li>Vanity installation</li>
       <li>Toilet seals and total replacements</li>
@@ -44,8 +64,8 @@ const listPlaceholder = [
     </ul>`,
   },
   {
-    id: 1,
-    title: 'In the Bathroom',
+    id: 5,
+    title: 'In the Bathroom5',
     description: `<ul>
       <li>Vanity installation</li>
       <li>Toilet seals and total replacements</li>
@@ -57,8 +77,8 @@ const listPlaceholder = [
     </ul>`,
   },
   {
-    id: 1,
-    title: 'In the Bathroom',
+    id: 6,
+    title: 'In the Bathroom6',
     description: `<ul>
       <li>Vanity installation</li>
       <li>Toilet seals and total replacements</li>
@@ -69,52 +89,49 @@ const listPlaceholder = [
       <li>Undersink plumbing and p-traps</li>
     </ul>`,
   },
-  {
-    id: 1,
-    title: 'In the Bathroom',
-    description: `<ul>
-      <li>Vanity installation</li>
-      <li>Toilet seals and total replacements</li>
-      <li>Updating wall fixtures</li>
-      <li>Grout</li>
-      <li>Shower door installation</li>
-      <li>Replacing bathroom fans</li>
-      <li>Undersink plumbing and p-traps</li>
-    </ul>`,
-  },
-
 ];
 
 const ServicesList = () =>
 {
-  const rowList = [];
+  const [ activeItem, setActiveItem ] = useState(null);
 
-  for (let i = 0; i < listPlaceholder.length; i += 3)
+  const handlerClick = (id) =>
   {
-    const row = listPlaceholder.slice(i, i + 3);
-    rowList.push(row);
-  }
+    if (activeItem === id) setActiveItem(null);
+    else setActiveItem(id);
+  };
 
-  console.log(rowList);
+  // const rowList = [];
+
+  // for (let i = 0; i < listPlaceholder.length; i += 3)
+  // {
+  //   const row = listPlaceholder.slice(i, i + 3);
+  //   rowList.push(row);
+  // }
 
   return (
     <section className={ s.container }>
       <Title>
         Services
       </Title>
-      <ul className={ s.list }>
-        {rowList.map((chunk, index) => (
-          <ul key={ `wrapper-${ index }` } className={ s.row }>
-            {chunk.map((item) => (
-              <ServicesCard
-                key={ item.id }
-                title={ item.title }
-                description={ item.description }
-              />
-            ))}
-          </ul>
+      {/* <div className={ s['list-container'] }> */}
+      {/* {rowList.map((chunk, index) => ( */}
+      <ul className={ cn(s['list-container'], {
+        [s.active]: activeItem !== null,
+      }) }
+      >
+        {listPlaceholder.map((item) => (
+          <ServicesCard
+            key={ item.id }
+            title={ item.title }
+            description={ item.description }
+            isActive={ activeItem === item.id }
+            onClick={ () => handlerClick(item.id) }
+          />
         ))}
       </ul>
+      {/* ))} */}
+      {/* </div> */}
     </section>
   );
 };

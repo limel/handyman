f0fb3acc48a1fc1cd1c20fcb2f2f099932d0a931
Sergey Nosenko <yalord96@gmail.com>
2023-06-06 22:28:06 +0300

    swiper

--- front-end/src/components/Reviews/Reviews.jsx
+++ front-end/src/components/Reviews/Reviews.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-nested-ternary */
 import { useEffect, useState, useId } from 'react';
+import useWindowWidth from '~/hooks/useWindowWidth';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import s from './Reviews.module.scss';
 import ReviewItem from '../UI/ReviewItem/ReviewItem';
 import ReviewsHead from './ReviewsHead';
+import Button from '../UI/Button/Button';
+import Title from '../UI/Title/Title';
+import 'swiper/css';
 
 const Reviews = ({ googleReviews, yelpReviews, thumbtackReviews }) => {
+  const windowWidth = useWindowWidth();
+  const reviewsCount = windowWidth >= 992 ? 10 : 2;
   const [ reviews, setReviews ] = useState([]);
-  const [ visibleReviews, setVisibleReviews ] = useState(10);
+  const [ visibleReviews, setVisibleReviews ] = useState(reviewsCount);
 
   useEffect(() => {
     if (!googleReviews || !yelpReviews || !thumbtackReviews) return;
@@ -18,26 +25,57 @@ const Reviews = ({ googleReviews, yelpReviews, thumbtackReviews }) => {
   }, [ googleReviews, yelpReviews, thumbtackReviews ]);
 
   const handleLoadMoreReviews = () => {
-    setVisibleReviews((prevCount) => prevCount + 10);
+    setVisibleReviews((prevCount) => prevCount + reviewsCount);
   };
 
   return (
     <section className={ s.section }>
-      <ReviewsHead />
+      <Title>Reviews</Title>
+      {windowWidth >= 992 ? <ReviewsHead /> : null}
+      <Swiper
+        className={ s.swiper }
+        spaceBetween={ 15 }
+        slidesPerView={ 2 }
+        direction="vertical"
+        autoHeight
+      >
+        {reviews.length !== 0 && (reviews.map((review, index) => (
+          <SwiperSlide className={ s['swiper-slide'] } key={ index }><ReviewItem { ...review } /></SwiperSlide>
+        )))}
+      </Swiper>
       <div className={ s.reviews }>
-        {reviews.length !== 0 ? (reviews.slice(0, visibleReviews).map((review, index) => (
-          <ReviewItem { ...review } key={ index } />
-        ))) : (
-          <p className={ s.fallback }>
-            Sorry, services is not aviable now, please try again later
-          </p>
+
+        {reviews.length === 0 && (
+        <p className={ s.fallback }>
+          Sorry, services is not aviable now, please try again later
+        </p>
         )}
       </div>
-      {visibleReviews < reviews.length && (
-      <button type="button" className={ s['load-more'] } onClick={ handleLoadMoreReviews }>
-        Load More Reviews
-      </button>
-      )}
+      <div className={ s.buttons }>
+        {windowWidth > 992
+          ? visibleReviews < reviews.length && (
+          <Button className={ s['load-more'] } onClick={ handleLoadMoreReviews }>
+            Load More Reviews
+          </Button>
+          )
+          : (
+            <div className={ s['load-more'] } onClick={ handleLoadMoreReviews }>
+              <div>
+                <svg><use href="/sprite.svg#arrow" /></svg>
+                <svg><use href="/sprite.svg#arrow" /></svg>
+              </div>
+              <div>
+                <svg><use href="/sprite.svg#arrow" /></svg>
+                <svg><use href="/sprite.svg#arrow" /></svg>
+              </div>
+            </div>
+          )}
+        <Button className={ s['google-review'] }>
+          LEAVE A REVIEW&nbsp;&nbsp;
+          <svg className={ s['link-icon'] }><use href="/sprite.svg#google" /></svg>
+        </Button>
+      </div>
+      <p className={ s['button-discription'] }>Google review</p>
     </section>
   );
 };
